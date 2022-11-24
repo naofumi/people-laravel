695dcf274bd53338057c5d585ffd33cc0b6918b0
Naofumi Kagami <naofumi@mac.com>
2022-11-24 20:34:58 +0900

    Fix up people#index with correct links

--- resources/views/people/index.blade.php
+++ resources/views/people/index.blade.php
@@ -3,8 +3,15 @@
 
 @section('content')
 
-<h1>People</h1>
-<div>
+<div class="d-flex justify-content-between">
+	<h1>People</h1>
+	<div>
+		<a href="{{ route('people.create') }}" class="btn btn-primary">
+			New Person
+		</a>
+	</div>
+</div>
+<div id="people">
 	<table class="table">
 		<thead>
 			<tr>
@@ -14,8 +21,12 @@
 		</thead>
 		<tbody>
 			@foreach ($people as $person)
-				<tr>
-					<td>{{ $person->name }}</td>
+				<tr id="person_{{ $person->id }}">
+					<td>
+						<a href="{{ route('people.show') }}">
+							{{ $person->name }}
+						</a>
+					</td>
 					<td>{{ $person->email }}</td>
 				</tr>
 			@endforeach
