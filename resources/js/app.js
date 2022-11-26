d4b1a8b5d65ba5c66fcb90dbf4f7e0eef8d32021
Naofumi Kagami <naofumi@mac.com>
2022-11-26 18:43:45 +0900

    php artisan breeze:install and stuff
    tried to run `php artisan breeze:install` but complained that some of the folders that were present in the initial setup but since removed were missing.
    1. resources/views/welcome.blade.php
    2. resources/css/app.css
    
    I restored this and ran `php artisan breeze:install`

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,24 +1,7 @@
 import './bootstrap';
 
-// https://laravel.com/docs/9.x/vite#blade-processing-static-assets
-import.meta.glob([
-  '../images/**',
-  '../fonts/**',
-]);
+import Alpine from 'alpinejs';
 
-import hotwiredTurbo from 'https://cdn.skypack.dev/@hotwired/turbo';
+window.Alpine = Alpine;
 
-import { Application, Controller } from "https://unpkg.com/@hotwired/stimulus/dist/stimulus.js"
-window.Stimulus = Application.start()
-
-Stimulus.register("people-search", class extends Controller {
-  static targets = [ "turboframe" ]
-
-  input(event) {
-    const action = event.target.form.action
-    const query = event.target.value
-    const url = action + '?search=' + query
-    this.turboframeTarget.src = url
-  }
-})
-
+Alpine.start();
