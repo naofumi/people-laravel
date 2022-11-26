4b9ffb4de824687ad31f68e15c3b4f5fa8f88e0d
Naofumi Kagami <naofumi@mac.com>
2022-11-26 18:50:29 +0900

    remove references to tailwind from laravel/breeze
    We prefer to use bootstrap

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,24 @@
 import './bootstrap';
 
-import Alpine from 'alpinejs';
+// https://laravel.com/docs/9.x/vite#blade-processing-static-assets
+import.meta.glob([
+  '../images/**',
+  '../fonts/**',
+]);
 
-window.Alpine = Alpine;
+import hotwiredTurbo from 'https://cdn.skypack.dev/@hotwired/turbo';
 
-Alpine.start();
+import { Application, Controller } from "https://unpkg.com/@hotwired/stimulus/dist/stimulus.js"
+window.Stimulus = Application.start()
+
+Stimulus.register("people-search", class extends Controller {
+  static targets = [ "turboframe" ]
+
+  input(event) {
+    const action = event.target.form.action
+    const query = event.target.value
+    const url = action + '?search=' + query
+    this.turboframeTarget.src = url
+  }
+})
+
