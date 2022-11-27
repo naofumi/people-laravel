821d03e61cce60a3ba2fecc576f6289efb54c466
Naofumi Kagami <naofumi@mac.com>
2022-11-27 16:10:36 +0900

    Put application routes behind auth

--- routes/web.php
+++ routes/web.php
@@ -16,17 +16,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [PersonController::class, 'index']);
-
-Route::resource('people', PersonController::class);
-Route::resource('users', UserController::class);
-Route::resource('notes', NoteController::class);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    Route::get('/', [PersonController::class, 'index']);
+
+    # Application routes
+    Route::resource('people', PersonController::class);
+    Route::resource('users', UserController::class);
+    Route::resource('notes', NoteController::class);
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
